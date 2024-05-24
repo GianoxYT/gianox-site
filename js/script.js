4dce7511b397256f5bf91f4f89b28ca7d9fed625
GianoxYT <gianoxtag.replay@gmail.com>
2024-05-24 22:40:33 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// Fonction pour récupérer les vidéos
+function getLatestVideos() {
+  const apiKey = 'AIzaSyC-07OK-2K6-lNTd8fopPuo1zDg0XX4K7s';
+  const channelId = 'UCsighiK1o2HW4PNosdVUKRw';
+  const maxResults = 3;
+  const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=${maxResults}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const videos = data.items.filter(item => item.id.kind === 'youtube#video' && !item.id.videoId.includes('short') && item.id.videoId.length > 11);
+      videos.forEach(video => {
+        const thumbnailUrl = video.snippet.thumbnails.medium.url;
+        const videoId = video.id.videoId;
+        const title = video.snippet.title;
+        displayVideoThumbnail(thumbnailUrl, videoId, title);
+      });
+    })
+    .catch(error => console.error('Erreur lors de la récupération des vidéos :', error));
+}
+
+// Fonction pour afficher les miniatures des vidéos
+function displayVideoThumbnail(thumbnailUrl, videoId, title) {
+  const thumbnailsContainer = document.getElementById('miniatures');
+  const thumbnailElement = document.createElement('div');
+  thumbnailElement.classList.add('miniature');
+  
+  const thumbnailLink = document.createElement('a');
+  thumbnailLink.href = `https://www.youtube.com/watch?v=${videoId}`;
+  thumbnailLink.target = '_blank';
+
+  const thumbnailImg = document.createElement('img');
+  thumbnailImg.src = thumbnailUrl;
+  thumbnailImg.alt = title;
+
+  thumbnailLink.appendChild(thumbnailImg);
+  thumbnailElement.appendChild(thumbnailLink);
+  thumbnailsContainer.appendChild(thumbnailElement);
+}
+
+// Appel de la fonction pour récupérer et afficher les vidéos au chargement de la page
+document.addEventListener('DOMContentLoaded', getLatestVideos);
